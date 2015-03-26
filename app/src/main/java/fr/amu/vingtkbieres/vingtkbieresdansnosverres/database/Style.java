8f9b54e6a94c7efefa99ef26a50da4bd392be960
Jonathan BAUDIN <jjbaudin@gmail.com>
2015-03-26 14:46:25 +0100

    Créion d'une classe pour le Style d'une bière

---
+++ app/src/main/java/fr/amu/vingtkbieres/vingtkbieresdansnosverres/database/Style.java
@@ -0,0 +1,14 @@
+package fr.amu.vingtkbieres.vingtkbieresdansnosverres.database;
+
+/**
+ * Created by legeek on 26/03/15.
+ */
+public class Style {
+    final public int id;
+    final public String text;
+
+    public Style(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+}
