32751514912d03ca6c9478262ebb7d522248b76f
WildRhum <kyon-04@hotmail.fr>
2015-04-09 13:31:52 +0200

    Revert "Suppression de la classe biere"

--- app/src/main/java/fr/amu/vingtkbieres/vingtkbieresdansnosverres/recherche/RechercheActivity.java
+++ app/src/main/java/fr/amu/vingtkbieres/vingtkbieresdansnosverres/recherche/RechercheActivity.java
@@ -1,124 +1,20 @@
 package fr.amu.vingtkbieres.vingtkbieresdansnosverres.recherche;
 
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import org.json.JSONException;
-
-import java.util.ArrayList;
 
 import fr.amu.vingtkbieres.vingtkbieresdansnosverres.R;
-import fr.amu.vingtkbieres.vingtkbieresdansnosverres.database.Database;
-import fr.amu.vingtkbieres.vingtkbieresdansnosverres.database.JSONDataException;
-import fr.amu.vingtkbieres.vingtkbieresdansnosverres.database.Style;
 
 public class RechercheActivity extends ActionBarActivity {
-    ArrayList<Style> arrayStyle = new ArrayList<>();
-    ArrayList<CheckBox> checkBox;
 
-    private class asyncDbTest extends AsyncTask< Void, Void, Void >{
-        ArrayList<Style> styles;
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                styles = new ArrayList<>( Database.getAllStyle() );
-                for( Style s : styles ){
-                    System.out.println( s.text );
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (JSONDataException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            arrayStyle = styles;
-            creerCheckBox();
-        }
-    }
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recherche);
-
-        asyncDbTest asyncTask = new asyncDbTest();
-        asyncTask.execute();
-
-//        for(int i = 0; i < arrayStyle.size(); ++i)
-//        {
-//            System.out.println("TEST VALEUR :" + arrayStyle.get(i).text);
-//        }
-//
-//        if (arrayStyle.isEmpty())
-//            System.out.println("ArrayStyle vide!");
-
-
-        final EditText edit = (EditText) findViewById(R.id.editTexteRechercheBiere);
-        Button b = (Button) findViewById(R.id.boutonRecherche);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                ArrayList<Style> styleChoisi = new ArrayList<Style>();
-
-                for(int i = 0; i < arrayStyle.size(); ++i)
-                {
-                    if(!arrayStyle.get(i).text.equals("null"))
-                    {
-                        if(checkBox.get(i).isChecked()){
-                            styleChoisi.add(arrayStyle.get(i));
-                        }
-                    }
-                }
-
-                Intent intent = new Intent(RechercheActivity.this, ResultatRechercheActivity.class);
-                // Si des styles ont été choisi
-                if(!styleChoisi.isEmpty()) {
-                    intent.putParcelableArrayListExtra("style", styleChoisi);
-                }
-                Toast.makeText(getApplicationContext(), "Pas de style choisi", Toast.LENGTH_SHORT).show();
-
-                intent.putParcelableArrayListExtra("style", styleChoisi);
-                startActivity(intent);
-            }
-        });
     }
 
-    private void creerCheckBox() {
-        checkBox = new ArrayList<>();
-        LinearLayout ll = (LinearLayout) findViewById(R.id.rechercheBiere);
-
-        // creation des checkBox
-        for(int i = 0; i < arrayStyle.size(); i++){
-            CheckBox newCheckBox = new CheckBox(this);
-            newCheckBox.setId(i);
-            newCheckBox.setText(arrayStyle.get(i).text);
-
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.setMargins(100,0,0,0);
-            newCheckBox.setLayoutParams(layoutParams);
-
-            checkBox.add(newCheckBox);
-
-            // ajout des checkBox au layout
-            ll.addView(newCheckBox);
-        }
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
